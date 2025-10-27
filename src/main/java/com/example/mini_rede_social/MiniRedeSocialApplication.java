ae9ea35dc83305919db09aa00786019cc3695730
PC JOSE FERNANDO <josefernandojosefernando.09@gmail.com>
2025-10-27 02:43:54 -0300

    chore: Inicializa estrutura do projeto para API Mini Rede Social
    Configura a estrutura inicial do projeto Maven, incluindo diretórios padrão de código-fonte e recursos (src/main/java, src/main/resources, src/test/java) e arquivos de configuração básicos (pom.xml, .gitignore).

---
+++ src/main/java/com/example/mini_rede_social/MiniRedeSocialApplication.java
@@ -0,0 +1,13 @@
+package com.example.mini_rede_social;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniRedeSocialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiniRedeSocialApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/mini_rede_social/MiniRedeSocialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mini_rede_social;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniRedeSocialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
