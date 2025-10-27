f6c264549a5ab90769cf315050de1d2e3906c703
PC JOSE FERNANDO <josefernandojosefernando.09@gmail.com>
2025-10-27 02:40:36 -0300

    feat(model): Cria a entidade PerfilModel com JPA e Lombok
    Define a entidade 'PerfilModel' para persistência com JPA.
    Inclui:
    - Relacionamento OneToOne com UsuarioModel usando chave primária compartilhada (@MapsId).
    - Campo 'dataNascimento' (LocalDate) para armazenar a data de nascimento.
    - Método transient '@Transient getIdade()' para calcular a idade dinamicamente.

---
+++ src/main/java/com/example/mini_rede_social/model/PerfilModel.java
@@ -0,0 +1,42 @@
+package com.example.mini_rede_social.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@Table(name = "perfil")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class PerfilModel {
+    @Id
+    private UUID id;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @MapsId
+    @JoinColumn(name = "usuario_id")
+    private UsuarioModel usuario;
+
+    @Column(nullable = false, length = 100)
+    private String nomeCompleto;
+    @Column()
+    private String bio;
+    @Column(nullable = false)
+    private LocalDate dataNascimento;
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime dataCriacao = LocalDateTime.now();
+
+    @Transient
+    public Integer getIdade() {
+        if (this.dataNascimento == null) {
+            return null;
+        }
+        return java.time.Period.between(this.dataNascimento, LocalDate.now()).getYears();
+    }
+}
